38ec95e3428b6b04169457b3b7a38fcdd542a4b8
KradoDev <alexisarmaos@hotmail.fr>
2017-12-11 15:03:23 +0100

    ListFile

---
+++ client/ListFileClient.java
@@ -0,0 +1,34 @@
+package client;
+import comServerClient.*;
+import java.io.*;
+import java.util.*;
+
+
+public class ListFileClient {
+    private TreeSet<P2PFile> fileList;
+    
+    public ListFileClient(File folder){
+        fileList=new TreeSet<P2PFile>();
+        listFilesFolder(folder);
+    }
+    
+    private void listFilesFolder(File folder) {
+        for (File f : folder.listFiles()) {
+            if (f.isDirectory()) {
+                listFilesFolder(f);
+            }
+            else{
+                fileList.add(new P2PFile(f));
+            }
+        }
+    }
+    
+    public void afficherList(){
+        for(P2PFile f : fileList){
+            System.out.println(f.getFile().getName()+"   "+f.getTaille()+" octets");
+        }
+    }
+    
+    public TreeSet<P2PFile> getFileList(){ return fileList; }
+}
+
